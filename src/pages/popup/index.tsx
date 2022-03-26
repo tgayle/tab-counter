05e830d759ac56f476318a3b5cf9b5cfed0d4630
tgayle <travisgayledev@gmail.com>
2022-03-26 18:38:48 -0400

    use createRoot instead of render for application setup.

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
 console.log('popup script');
-
 const root = document.querySelector('#root');
-
-render(<App />, root);
+createRoot(root!).render(<App />);
