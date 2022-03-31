3181b7bf252944c296aef42eb59592a56fc56841
tgayle <travisgayledev@gmail.com>
2022-03-31 14:39:44 -0400

    Fix error due to useCurrentWindow resolving after component dismount.

--- src/hooks/useCurrentWindow.ts
+++ src/hooks/useCurrentWindow.ts
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
+import { BrowserWindow } from '../tabutil';
 
 export function useCurrentWindow() {
-  const [window, setWindow] = useState<chrome.windows.Window | null>(null);
+  const [window, setWindow] = useState<BrowserWindow | null>(null);
 
   useEffect(() => {
+    let active = true;
     (async () => {
       const currentWindow = await chrome.windows.getCurrent();
+      if (!active) return;
       setWindow(currentWindow);
     })();
+
+    return () => {
+      active = false;
+    };
   }, []);
 
   return window;
