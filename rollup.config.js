8aafec3c096ffe1eb242020c771e27667c054212
tgayle <travisgayledev@gmail.com>
2022-05-01 23:10:25 -0400

    feat: switch to preact from react

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import path from 'path';
 
+import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-
 import {
   chromeExtension,
   simpleReloader,
@@ -22,6 +22,14 @@ export default {
     chunkFileNames: path.join('chunks', '[name]-[hash].js'),
   },
   plugins: [
+    alias({
+      entries: [
+        { find: 'react', replacement: 'preact/compat' },
+        { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+        { find: 'react-dom', replacement: 'preact/compat' },
+        { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
+      ]
+    }),
     replace({
       'process.env.NODE_ENV': isProduction
         ? JSON.stringify('production')
