0a6c2071c3eb18bc7e94e646d2d8d638e65b1a63
Travis Gayle <tgayle@users.noreply.github.com>
2026-08-05 18:24:49 -0700

    fix: Github actions for playwright tests (#6)

--- tests/base.ts
+++ tests/base.ts
@@ -15,6 +15,7 @@ export const test = base.extend<{
 
     const context = await chromium.launchPersistentContext('', {
       channel: 'chromium',
+      reducedMotion: 'reduce',
       args: [
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`,
